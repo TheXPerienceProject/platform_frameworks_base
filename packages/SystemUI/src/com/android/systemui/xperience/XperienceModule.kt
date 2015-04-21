36d5e2adeed7cf2b7fffadc06cdf4db5dbbdef2a
Michael Bestas <mikeioannina@gmail.com>
2015-04-22 02:15:28 +0300

    SystemUI: Add heads up tile
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I0e3cff73752c59d804dcec43b86524c2e8df4f57
    Signed-off-by: klozz <carlosj@klozz.dev>

--- packages/SystemUI/src/com/android/systemui/xperience/XperienceModule.kt
+++ packages/SystemUI/src/com/android/systemui/xperience/XperienceModule.kt
@@ -19,6 +19,7 @@ package com.android.systemui.xperience
 import com.android.systemui.qs.QsEventLogger
 import com.android.systemui.qs.pipeline.shared.TileSpec
 import com.android.systemui.qs.shared.model.TileCategory
+import com.android.systemui.qs.tiles.HeadsUpTile
 import com.android.systemui.qs.tileimpl.QSTileImpl
 import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.base.shared.model.QSTileConfig
@@ -39,8 +40,15 @@ interface XperienceModule {
     @StringKey(CaffeineTile.TILE_SPEC)
     fun bindCaffeineTile(caffeineTile: CaffeineTile): QSTileImpl<*>
 
+    /** Inject HeadsUpTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(HeadsUpTile.TILE_SPEC)
+    fun bindHeadsUpTile(headsUpTile: HeadsUpTile): QSTileImpl<*>
+
     companion object {
         const val CAFFEINE_TILE_SPEC = "caffeine"
+	const val HEADS_UP_TILE_SPEC = "heads_up"
 
         @Provides
         @IntoMap
@@ -56,6 +64,19 @@ interface XperienceModule {
                 instanceId = uiEventLogger.getNewInstanceId(),
                 category = TileCategory.DISPLAY,
             )
-
+        @Provides
+        @IntoMap
+        @StringKey(HEADS_UP_TILE_SPEC)
+        fun provideHeadsUpTileConfig(uiEventLogger: QsEventLogger): QSTileConfig =
+            QSTileConfig(
+                tileSpec = TileSpec.create(HEADS_UP_TILE_SPEC),
+                uiConfig =
+                    QSTileUIConfig.Resource(
+                        iconRes = R.drawable.ic_qs_heads_up,
+                        labelRes = R.string.quick_settings_heads_up_label
+                    ),
+                instanceId = uiEventLogger.getNewInstanceId(),
+                category = TileCategory.ACCESSIBILITY,
+            )
     }
 }
