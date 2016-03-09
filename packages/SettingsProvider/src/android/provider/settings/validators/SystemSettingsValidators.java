423ba12aa9ed94d72a9672dadbe25288c69cb1b4
Danesh M <daneshm90@gmail.com>
2016-03-09 11:48:43 -0800

    Add high touch sensitivity and hovering to InputService
    Change-Id: I5a6af73129acefa6530ceb3f73cc4cd83a19a676
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: klozz <carlosj@klozz.dev>

--- packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
@@ -288,5 +288,6 @@ public class SystemSettingsValidators {
                 System.ACCESSIBILITY_FORCE_INVERT_COLOR_OVERRIDE_PACKAGES_TO_DISABLE,
                 new PackageNameListValidator(","));
         VALIDATORS.put(System.TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK, BOOLEAN_VALIDATOR);
+        VALIDATORS.put(System.HIGH_TOUCH_SENSITIVITY_ENABLE, BOOLEAN_VALIDATOR);
     }
 }
