c4a5dca16403336ec6ddf2c434dda915945487e6
Jake Weinstein <xboxlover360@gmail.com>
2016-10-13 23:56:02 -0400

    base: set scrolling friction to 0.006f
    Improves responsiveness
    
    Change-Id: I18a52ac84ba9c8274adc757e41ca2ca995d514f6
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/widget/AbsListView.java
+++ core/java/android/widget/AbsListView.java
@@ -4991,6 +4991,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
 
         FlingRunnable() {
             mScroller = new OverScroller(getContext());
+            mScroller.setFriction(0.006f);
         }
 
         float getSplineFlingDistance(int velocity) {
