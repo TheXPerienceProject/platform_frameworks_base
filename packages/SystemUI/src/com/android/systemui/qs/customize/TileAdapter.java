81a9170891235812a681d6f7db49a64b6fd04066
Andrzej Ressel <jereksel@gmail.com>
2017-08-09 00:11:16 +0200

    SystemUI: Allow user to add/remove QS with one click
    Change-Id: Ied86dcfb6179d936786273867a50610900f1c5e5
    Signed-off-by: klozz <carlosj@klozz.dev>

--- packages/SystemUI/src/com/android/systemui/qs/customize/TileAdapter.java
+++ packages/SystemUI/src/com/android/systemui/qs/customize/TileAdapter.java
@@ -23,9 +23,11 @@ import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.os.Handler;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnLayoutChangeListener;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.TextView;
@@ -112,7 +114,7 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
     private int mAccessibilityAction = ACTION_NONE;
     private int mAccessibilityFromIndex;
     private final UiEventLogger mUiEventLogger;
-    //private final AccessibilityDelegateCompat mAccessibilityDelegate;
+    private final AccessibilityDelegateCompat mAccessibilityDelegate;
     @Nullable
     private RecyclerView mRecyclerView;
     private int mNumColumns;
@@ -140,7 +142,7 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
                 ? context.getResources().getInteger(
                         R.integer.small_land_lockscreen_quick_settings_num_columns)
                 : context.getResources().getInteger(NUM_COLUMNS_ID);
-        //mAccessibilityDelegate = new TileAdapterDelegate();
+        mAccessibilityDelegate = new TileAdapterDelegate();
         mSizeLookup.setSpanIndexCacheEnabled(true);
         mTempTextView = new TextView(context);
         mMinTileViewHeight = context.getResources().getDimensionPixelSize(R.dimen.qs_tile_height);
@@ -431,28 +433,14 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
         // Don't show the side view for third party tiles, as we don't have the actual state.
         tileView.setShowSideView(position < mEditIndex || info.isSystem);
         holder.mTileView.setSelected(true);
-        /*holder.mTileView.setImportantForAccessibility(View.IMPORTANT_FOR_ACCESSIBILITY_YES);
+        holder.mTileView.setImportantForAccessibility(View.IMPORTANT_FOR_ACCESSIBILITY_YES);
         holder.mTileView.setClickable(true);
         holder.mTileView.setOnClickListener(null);
         holder.mTileView.setFocusable(true);
         holder.mTileView.setFocusableInTouchMode(true);
-        holder.mTileView.setAccessibilityTraversalBefore(View.NO_ID);*/
+        holder.mTileView.setAccessibilityTraversalBefore(View.NO_ID);
 
-        holder.mTileView.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int position = holder.getLayoutPosition();
-                if (position < mEditIndex) {
-                    if (canRemoveTiles()) {
-                        move(position, mEditIndex);
-                    }
-                } else {
-                    move(position, mEditIndex);
-                }
-            }
-        });
-
-        /*if (mAccessibilityAction != ACTION_NONE) {
+        if (mAccessibilityAction != ACTION_NONE) {
             holder.mTileView.setClickable(selectable);
             holder.mTileView.setFocusable(selectable);
             holder.mTileView.setFocusableInTouchMode(selectable);
@@ -474,7 +462,19 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
         }
         if (position == mFocusIndex) {
             focusOnHolder(holder);
-        }*/
+        }
+        holder.mTileView.setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent ev) {
+                if (ev.getAction() == MotionEvent.ACTION_UP) {
+                    int position = holder.getLayoutPosition();
+                    if (position >= mEditIndex || canRemoveTiles()) {
+                        move(position, mEditIndex, true);
+                    }
+                }
+                return false;
+            }
+        });
     }
 
     private void focusOnHolder(Holder holder) {
@@ -635,7 +635,7 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
                 mTileView = (QSTileViewImpl) ((FrameLayout) itemView).getChildAt(0);
                 mTileView.getIcon().disableAnimation();
                 mTileView.setTag(this);
-                //ViewCompat.setAccessibilityDelegate(mTileView, mAccessibilityDelegate);
+                ViewCompat.setAccessibilityDelegate(mTileView, mAccessibilityDelegate);
             }
         }
 
