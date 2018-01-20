e094b9067a00091f651904ab7393b99d1adcb49e
Michael Bestas <mkbestas@lineageos.org>
2018-01-21 00:22:10 +0200

    frameworks: Add support for advanced restart options
    dont move to fking separated repo
    
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: Dave Kessler <activethrasher00@gmail.com>
    Co-authored-by: DvTonder <david.vantonder@gmail.com>
    Co-authored-by: Jorge Ruesga <jorge@ruesga.com>
    Co-authored-by: maxwen <max.weninger@gmail.com>
    Co-authored-by: Ricardo Cerqueira <cyanogenmod@cerqueira.org>
    Co-authored-by: Roman Birg <roman@cyngn.com>
    Co-authored-by: SagarMakhar <sagarmakhar@gmail.com>
    Co-authored-by: Veeti Paananen <veeti.paananen@rojekti.fi>
    Co-authored-by: Zhao Wei Liew <zhaoweiliew@gmail.com>
    Change-Id: I4b35d10b0811eff0d8e9ab68b2aedbe52b4aa53f
    enable by default im not gona add menu for this >:(
    Signed-off-by: klozz <carlosj@klozz.dev>

---
+++ core/java/com/android/internal/xpe/util/PowerMenuUtils.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.internal.xpe.util;
+
+import android.app.KeyguardManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import static com.android.internal.xpe.util.PowerMenuConstants.*;
+
+public final class PowerMenuUtils {
+
+public static boolean isAdvancedRestartPossible(final Context context) {
+        KeyguardManager km = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
+        boolean keyguardLocked = km.inKeyguardRestrictedInputMode() && km.isKeyguardSecure();
+        boolean advancedRestartEnabled = Settings.Secure.getInt(context.getContentResolver(),
+                Settings.Secure.ADVANCED_REBOOT, 1) == 1;
+        boolean isPrimaryUser = UserHandle.getCallingUserId() == UserHandle.USER_SYSTEM;
+        return advancedRestartEnabled && !keyguardLocked && isPrimaryUser;
+    }
+
+    public static boolean isPanicAvailable(final Context context) {
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName(PANIC_PACKAGE, PANIC_ACTIVITY));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        if (context.getPackageManager().resolveActivity(intent,
+                PackageManager.MATCH_SYSTEM_ONLY) != null) {
+            return true;
+        }
+        return false;
+    }
+
+}
