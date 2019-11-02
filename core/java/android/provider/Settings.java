1b2523fb7151d5c84cae245b73c85bc84309f154
ezio84 <brabus84@gmail.com>
2019-11-02 17:46:14 +0100

    Ambient Music Ticker - Allow to pulse on new tracks [1/2]
    Change-Id: I64595b2689e9340fd43c75e9a19ba79702fd9333
    Signed-off-by: klozz <carlosj@klozz.dev>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13307,6 +13307,12 @@ public final class Settings {
         @Readable
         public static final String KEYBOX_DATA_TIMESTAMP = "keybox_data_timestamp";
 
+	/**
+         * Whether to pulse ambient on new music tracks
+         * @hide
+         */
+        public static final String PULSE_ON_NEW_TRACKS = "pulse_on_new_tracks";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
