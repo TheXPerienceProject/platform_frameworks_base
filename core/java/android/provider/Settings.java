6f981a9562f0a9f5e3ceb4b630f18d854fd1d8ef
Arian <arian.kulmer@web.de>
2021-01-03 16:49:50 +0100

    LineageHardwareManager: Introduce keys swap feature support
    jhenrique09 edits: Adapt to AOSP
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Change-Id: I789814d780b06b146c1f9ab62293f4d6ca287208
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: klozz <carlosj@klozz.dev>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13549,6 +13549,12 @@ public final class Settings {
         public static final String HAPTIC_EFFECTS_PROFILE = "haptic_effects_profile";
 
         /**
+         * Whether key swap is enabled on supported hardware
+         * @hide
+         */
+        public static final String SWAP_CAPACITIVE_KEYS = "swap_capacitive_keys";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
