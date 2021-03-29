f9a156f3d960f64c413ea83c6960aa66dd4ebc38
Gegham Zakaryan <zakaryan.2004@outlook.com>
2021-03-29 21:59:00 +0400

    frameworks: Add more power menu customizations
    * Forward-port the rest of power menu customizations (airplane mode,
      screenshot, lockdown, bugreport, user switcher).
    * Screenshot option will always show by default and can be turned off
      in Settings.
    * Use the new LineageGlobalActionsService to get user's configuration
    
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: Dhina17 <dhinalogu@gmail.com>
    Co-authored-by: Michael Bestas <mikeioannina@gmail.com>
    Change-Id: I225037422937602940aafb5d20acea8c0bcff6ba
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: klozz <carlosj@klozz.dev>

--- packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
@@ -522,6 +522,7 @@ public class SecureSettingsValidators {
                 new InclusiveIntegerRangeValidator(0, 1));
         VALIDATORS.put(Secure.HAPTIC_EFFECTS_PROFILE, ANY_STRING_VALIDATOR);
         VALIDATORS.put(Secure.KEYBOX_DATA, ANY_STRING_VALIDATOR);
-	VALIDATORS.put(Secure.ADVANCED_REBOOT, BOOLEAN_VALIDATOR);
+        VALIDATORS.put(Secure.ADVANCED_REBOOT, BOOLEAN_VALIDATOR);
+        VALIDATORS.put(Secure.POWER_MENU_ACTIONS, ANY_STRING_VALIDATOR);
     }
 }
