243545248081d5f4a1c57a6a8765a497a6b3a8a7
Pranav Vashi <neobuddy89@gmail.com>
2021-06-26 09:48:39 +0530

    BatteryService: Add support for oem fast charger detection
    This is based on commit by amartinz:
    https://review.lineageos.org/c/LineageOS/android_frameworks_base/+/207583
    
    This supports dash, warp, vooc, turbo charging.
    
    Added config_oemFastChargerStatusPath and config_oemFastChargerStatusPath2
    as OEM can provide separate paths for wired and wireless fast charging.
    
    Change-Id: If9eeda0fa65666b39fa226c9c0083b6917810561
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    
    Add supervooc
    Change-Id: I710c75ee90ad4f97a7b3629cdd90e3c250e60c83
    Signed-off-by: klozz <carlosj@klozz.dev>

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -246,6 +246,11 @@ public class KeyguardIndicationController {
 
     private int mCurrentDivider;
 
+    private boolean mHasDashCharger;
+    private boolean mHasWarpCharger;
+    private boolean mHasVoocCharger;
+    private boolean mHasSuperVoocCharger;
+
     private KeyguardUpdateMonitorCallback mUpdateMonitorCallback;
 
     private boolean mDozing;
@@ -412,6 +417,15 @@ public class KeyguardIndicationController {
                 TAG,
                 mHandler
         );
+
+        mHasDashCharger = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_hasDashCharger);
+        mHasWarpCharger = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_hasWarpCharger);
+        mHasVoocCharger = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_hasVoocCharger);
+        mHasSuperVoocCharger = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_hasSuperVoocCharger);
     }
 
     /** Call this after construction to finish setting up the instance. */
@@ -1250,6 +1264,29 @@ public class KeyguardIndicationController {
         int chargingId;
         if (mPowerPluggedInWired) {
             switch (mChargingSpeed) {
+                case BatteryStatus.CHARGING_OEM:
+                    if (mHasDashCharger) {
+                        chargingId = hasChargingTime
+                                ? R.string.keyguard_indication_dash_charging_time
+                                : R.string.keyguard_plugged_in_dash_charging;
+                    } else if (mHasWarpCharger) {
+                        chargingId = hasChargingTime
+                                ? R.string.keyguard_indication_warp_charging_time
+                                : R.string.keyguard_plugged_in_warp_charging;
+                    } else if (mHasVoocCharger) {
+                        chargingId = hasChargingTime
+                                ? R.string.keyguard_indication_vooc_charging_time
+                                : R.string.keyguard_plugged_in_vooc_charging;
+                    } else if (mHasSuperVoocCharger) {
+                        chargingId = hasChargingTime
+                                ? R.string.keyguard_indication_supervooc_charging_time
+                                : R.string.keyguard_plugged_in_supervooc_charging;
+                    } else {
+                        chargingId = hasChargingTime
+                                ? R.string.keyguard_indication_turbo_power_time
+                                : R.string.keyguard_plugged_in_turbo_charging;
+                    }
+                    break;
                 case BatteryStatus.CHARGING_FAST:
                     chargingId = hasChargingTime
                             ? R.string.keyguard_indication_charging_time_fast
