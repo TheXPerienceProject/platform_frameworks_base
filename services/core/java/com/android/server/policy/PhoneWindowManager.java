d3e6a4417c49f84c58979355bf14f9a71c8bcc45
jhonboy121 <alfredmathew05@gmail.com>
2021-12-17 11:09:37 +0530

    base: allow toggling torch by double/long pressing power button
    * This is acheived by extending existing gestures for camera / assistant, power menu open is not delayed.
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: klozz <carlosj@klozz.dev>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -243,6 +243,7 @@ import com.android.internal.policy.PhoneWindow;
 import com.android.internal.policy.TransitionAnimation;
 import com.android.internal.statusbar.IStatusBarService;
 import com.android.internal.widget.LockPatternUtils;
+import com.android.internal.util.xperience.XperienceUtils;
 import com.android.server.AccessibilityManagerInternal;
 import com.android.server.DockObserverInternal;
 import com.android.server.ExtconStateObserver;
@@ -621,7 +622,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private boolean mShouldEarlyShortPressOnPower;
     boolean mShouldEarlyShortPressOnStemPrimary;
     int mLongPressOnPowerBehavior;
-    long mLongPressOnPowerAssistantTimeoutMs;
+    private boolean mTorchLongPressPowerEnabled;
+    long mLongPressOnPowerTimeoutMs;
     int mVeryLongPressOnPowerBehavior;
     int mDoublePressOnPowerBehavior;
     ComponentName mPowerDoublePressTargetActivity;
@@ -1013,6 +1015,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.SWIPE_TO_SCREENSHOT), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.TORCH_LONG_PRESS_POWER), false, this,
                     UserHandle.USER_ALL);
             updateSettings();
         }
@@ -1626,6 +1631,25 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 mPocketManager.setListeningExternal(false);
                 break;
         }
+    }
+
+    private boolean powerLongPressGestures(long eventTime, boolean nonInteractive) {
+        if (DEBUG_INPUT) Slog.d(TAG, "powerLongPressGestures: eventTime = " + eventTime
+                + ", nonInteractive = " + nonInteractive +
+                ", mTorchLongPressPowerEnabled = " + mTorchLongPressPowerEnabled);
+        if (mTorchLongPressPowerEnabled && (nonInteractive || isFlashlightOn())) {
+            performHapticFeedback(HapticFeedbackConstants.LONG_PRESS,
+                    "Power - Long Press - Toggle flashlight");
+            XperienceUtils.toggleCameraFlash();
+            mPowerKeyHandled = true;
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isFlashlightOn() {
+        return Settings.Secure.getInt(mContext.getContentResolver(),
+            Settings.Secure.FLASHLIGHT_ENABLED, 0) == 1;
     }
 
     private void powerVeryLongPress() {
@@ -2574,7 +2598,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         mLongPressOnPowerBehavior = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_longPressOnPowerBehavior);
-        mLongPressOnPowerAssistantTimeoutMs = mContext.getResources().getInteger(
+        mLongPressOnPowerTimeoutMs = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_longPressOnPowerDurationMs);
         mVeryLongPressOnPowerBehavior = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_veryLongPressOnPowerBehavior);
@@ -2899,8 +2923,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         @Override
         long getLongPressTimeoutMs() {
-            if (getResolvedLongPressOnPowerBehavior() == LONG_PRESS_POWER_ASSISTANT) {
-                return mLongPressOnPowerAssistantTimeoutMs;
+            if (getResolvedLongPressOnPowerBehavior() == LONG_PRESS_POWER_ASSISTANT
+                    || mTorchLongPressPowerEnabled) {
+                return mLongPressOnPowerTimeoutMs;
             } else {
                 return super.getLongPressTimeoutMs();
             }
@@ -2908,11 +2933,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         @Override
         void onLongPress(long eventTime) {
-            if (mSingleKeyGestureDetector.beganFromNonInteractive()
-                    && !mSupportLongPressPowerWhenNonInteractive) {
+            final boolean nonInteractive = mSingleKeyGestureDetector.beganFromNonInteractive();
+            if (nonInteractive && !mSupportLongPressPowerWhenNonInteractive) {
                 Slog.v(TAG, "Not support long press power when device is not interactive.");
                 return;
             }
+            if (powerLongPressGestures(eventTime, nonInteractive)) return;
 
             powerLongPress(eventTime);
         }
@@ -3284,7 +3310,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 mVeryLongPressOnPowerBehavior = veryLongPressOnPowerBehavior;
             }
 
-            mLongPressOnPowerAssistantTimeoutMs = Settings.Global.getLong(
+            mLongPressOnPowerTimeoutMs = Settings.Global.getLong(
                     mContext.getContentResolver(),
                     Settings.Global.POWER_BUTTON_LONG_PRESS_DURATION_MS,
                     mContext.getResources().getInteger(
@@ -3322,6 +3348,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             mStylusButtonsEnabled = Settings.Secure.getIntForUser(resolver,
                     Secure.STYLUS_BUTTONS_ENABLED, 1, UserHandle.USER_CURRENT) == 1;
             mInputManagerInternal.setStylusButtonMotionEventsEnabled(mStylusButtonsEnabled);
+
+            mTorchLongPressPowerEnabled = Settings.Secure.getIntForUser(resolver,
+                    Settings.Secure.TORCH_LONG_PRESS_POWER, 0,
+                    UserHandle.USER_CURRENT) == 1;
 
             kidsModeEnabled = Settings.Secure.getIntForUser(resolver,
                     Settings.Secure.NAV_BAR_KIDS_MODE, 0, UserHandle.USER_CURRENT) == 1;
@@ -7441,8 +7471,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         pw.print("mSettingsKeyBehavior=");
         pw.println(settingsKeyBehaviorToString(mSettingsKeyBehavior));
         pw.print(prefix);
-        pw.print("mLongPressOnPowerAssistantTimeoutMs=");
-        pw.println(mLongPressOnPowerAssistantTimeoutMs);
+                pw.print("mTorchLongPressPowerEnabled=");
+                pw.println(String.valueOf(mTorchLongPressPowerEnabled));
+        pw.print(prefix);
+        pw.print("mLongPressOnPowerTimeoutMs=");
+        pw.println(mLongPressOnPowerTimeoutMs);
         pw.print(prefix);
                 pw.print("mVeryLongPressOnPowerBehavior=");
                 pw.println(veryLongPressOnPowerBehaviorToString(mVeryLongPressOnPowerBehavior));
