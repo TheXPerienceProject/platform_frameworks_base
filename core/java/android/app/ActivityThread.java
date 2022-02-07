d96591b0faa9207a5b654f12421190a2f62a6bc4
李欢欢 <lihuan3@xiaomi.com>
2022-02-07 21:12:45 +0800

    Fix the NullPointerException in Android S Version:mServicesData
    mServicesData was create in android s version,and it is assigned the same
    time with mServices,but there is not the same code to guarantee values in
    mServicesData not null
    https://issuetracker.google.com/issues/218161565
    Test: start app from the push service and not crash
    
    Change-Id: I2d7d2fe1fbb9d433cf2079c690c876dc96a1ab5e
    Signed-off-by: 李欢欢 <lihuan3@xiaomi.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -5305,7 +5305,7 @@ public final class ActivityThread extends ClientTransactionHandler
         Service s = mServices.get(data.token);
         if (DEBUG_SERVICE)
             Slog.v(TAG, "handleBindService s=" + s + " rebind=" + data.rebind);
-        if (s != null) {
+        if (s != null && createData != null) {
             try {
                 data.intent.setExtrasClassLoader(s.getClassLoader());
                 data.intent.prepareToEnterProcess(isProtectedComponent(createData.info),
@@ -5336,7 +5336,7 @@ public final class ActivityThread extends ClientTransactionHandler
     private void handleUnbindService(BindServiceData data) {
         CreateServiceData createData = mServicesData.get(data.token);
         Service s = mServices.get(data.token);
-        if (s != null) {
+        if (s != null && createData != null) {
             try {
                 data.intent.setExtrasClassLoader(s.getClassLoader());
                 data.intent.prepareToEnterProcess(isProtectedComponent(createData.info),
@@ -5444,7 +5444,7 @@ public final class ActivityThread extends ClientTransactionHandler
     private void handleServiceArgs(ServiceArgsData data) {
         CreateServiceData createData = mServicesData.get(data.token);
         Service s = mServices.get(data.token);
-        if (s != null) {
+        if (s != null && createData != null) {
             try {
                 if (data.args != null) {
                     data.args.setExtrasClassLoader(s.getClassLoader());
