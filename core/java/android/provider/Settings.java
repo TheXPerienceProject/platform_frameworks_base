7fd41eb8f9730280bb2b7f52274bcd7ade991ef7
Ido Ben-Hur <idoybh2@gmail.com>
2023-06-07 23:59:41 +0300

    base: services: Introduce AutoDimService [1/2]
    Exactly like AutoAODService but for REDUCE_BRIGHT_COLORS_ACTIVATED
    
    Change-Id: I7042f673ac9d6c910d5db1e2d5f8a0895be78e3a
    Signed-off-by: klozz <carlosj@klozz.dev>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13631,6 +13631,16 @@ public final class Settings {
         public static final String SWAP_CAPACITIVE_KEYS = "swap_capacitive_keys";
 
         /**
+         * The custom time extra dim should be on at
+         * Only relevant when {@link EXTRA_DIM_AUTO_MODE} is set to 2 and above
+         * 0 = Disabled (default)
+         * format: HH:mm,HH:mm (since,till)
+         * @hide
+         */
+        @Readable
+        public static final String EXTRA_DIM_AUTO_TIME = "extra_dim_auto_time";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
