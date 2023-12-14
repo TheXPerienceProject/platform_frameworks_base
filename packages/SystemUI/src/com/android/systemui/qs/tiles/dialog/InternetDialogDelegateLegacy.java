a15f152bf8ccea0cd2b3f1fe3275549e5e3aa5ca
Pranav Vashi <neobuddy89@gmail.com>
2023-12-14 12:36:33 +0530

    InternetDialog: Add missing setOnClickListener for data toggle
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: klozz <carlosj@klozz.dev>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialogDelegateLegacy.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialogDelegateLegacy.java
@@ -450,6 +450,7 @@ public class InternetDialogDelegateLegacy implements
         }
         mLifecycleRegistry.setCurrentState(Lifecycle.State.DESTROYED);
         mMobileNetworkLayout.setOnClickListener(null);
+        mMobileDataToggle.setOnClickListener(null);
         mConnectedWifListLayout.setOnClickListener(null);
         if (mSecondaryMobileNetworkLayout != null) {
             mSecondaryMobileNetworkLayout.setOnClickListener(null);
