ae23cbaf3e52fefdaacf94f60852c5d162df3e1d
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-08-08 20:14:44 +0530

    services: Add freeform system service
    Change-Id: Icb3933846fc38f7fe1679e7f1fd916c2c137671d

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -177,6 +177,7 @@ import com.android.server.criticalevents.CriticalEventLog;
 import com.android.server.devicepolicy.DevicePolicyManagerService;
 import com.android.server.devicestate.DeviceStateManagerService;
 import com.android.server.display.DisplayManagerService;
+import com.android.server.display.FreeformService;
 import com.android.server.display.color.ColorDisplayService;
 import com.android.server.dreams.DreamManagerService;
 import com.android.server.emergency.EmergencyAffordanceService;
@@ -2868,6 +2869,10 @@ public final class SystemServer implements Dumpable {
                 mSystemServiceManager.startService(AuthenticationPolicyService.class);
                 t.traceEnd();
             }
+
+            t.traceBegin("FreeformService");
+            mSystemServiceManager.startService(FreeformService.class);
+            t.traceEnd();
 
             if (!isWatch) {
                 t.traceBegin("StartPruneInstantAppsJobService");
