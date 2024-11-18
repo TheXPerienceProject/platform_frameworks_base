ec73b37f6a9ea1a1e6de8b51d831b8288b25c656
klozz <carlosj@klozz.dev>
2024-11-18 17:07:26 -0600

    implement device constants
    required for three finger
    
    Signed-off-by: klozz <carlosj@klozz.dev>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -235,6 +235,7 @@ import com.android.internal.policy.PhoneWindow;
 import com.android.internal.policy.TransitionAnimation;
 import com.android.internal.statusbar.IStatusBarService;
 import com.android.internal.widget.LockPatternUtils;
+import com.android.internal.xpe.util.DeviceKeysConstants;
 import com.android.server.AccessibilityManagerInternal;
 import com.android.server.ExtconStateObserver;
 import com.android.server.ExtconUEventObserver;
@@ -6455,7 +6456,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             @Override
             public void onSwipeThreeFingers() {
                 Action threeFingersSwipeAction = Action.fromSettings(mContext.getContentResolver(),
-                        LineageSettings.System.KEY_THREE_FINGERS_SWIPE_ACTION,
+                        Settings.System.KEY_THREE_FINGERS_SWIPE_ACTION,
                         Action.fromIntSafe(12));
                 if (threeFingersSwipeAction == Action.NOTHING)
                     return;
@@ -6470,7 +6471,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             @Override
             public void onLongPressThreeFingers() {
                 Action threeFingersLongPressAction = Action.fromSettings(mContext.getContentResolver(),
-                        LineageSettings.System.KEY_THREE_FINGERS_LONG_PRESS_ACTION,
+                        Settings.System.KEY_THREE_FINGERS_LONG_PRESS_ACTION,
                         Action.NOTHING);
                 if (threeFingersLongPressAction == Action.NOTHING)
                     return;
