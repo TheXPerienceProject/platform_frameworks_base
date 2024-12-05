665aac5bd58ce4e1ae67dd23d8124080b3b77f95
minaripenguin <minaripenguin@users.noreply.github.com>
2024-12-06 07:42:48 +0800

    services: Fix and improve three finger gestures touch event handling
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6951,7 +6951,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             public void onSwipeThreeFingers() {
                 Action threeFingersSwipeAction = Action.fromSettings(mContext.getContentResolver(),
                         LineageSettings.System.KEY_THREE_FINGERS_SWIPE_ACTION,
-                        Action.fromIntSafe(12));
+                        Action.NOTHING);
                 if (threeFingersSwipeAction == Action.NOTHING)
                     return;
                 long now = SystemClock.uptimeMillis();
