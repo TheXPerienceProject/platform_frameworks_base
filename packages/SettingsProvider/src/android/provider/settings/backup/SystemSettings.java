DerpFest-AOSP/frameworks_base

SystemUI: Bring back good ol' circle battery style
* This code is based on the old Java implementation and converted to Kotlin

* Instead of creating different style within the same drawable
  create one on its own, this makes the code more readable and future proof

idoybh:
* Add the settings value to backup array yet again
* update .gitignore (unrelated)
* Adapted to Sv2, A14

Squashed with:

    From: Ido Ben-Hur <idoybh2@gmail.com>
    Date: Thu, 6 Jan 2022 20:45:48 +0200
    Subject: BatteryMeterView: Allow disabling QS battery estimates [1/2]

    Adapted to Sv2

    Change-Id: I0a14124d7f97ea12a98ab0a8feed06508f180c6b

    From: Ido Ben-Hur <idoybh2@gmail.com>
    Date: Sat, 28 Nov 2020 19:36:51 +0200
    Subject: BatteryMeterView: Remove the icon view if we are not using it

    Having the view while not really using it (vis is GONE) is expansive.

    Also improve some code while at it:
    * Properly support multi users
    * Use the settings observer instead of tunables
    * Fix AOSP derp for estimates, where it would not be registered for change after a user switch
    * Properly document the settings string
    * Misc improvements

    Change-Id: If35b003176234c6a2aef0a4b09ab166d87f6052a

    From: Ido Ben-Hur <idoybh2@gmail.com>
    Date: Sun, 29 Nov 2020 02:08:20 +0200
    Subject: base: Allow choosing whether to show % inside battery icon [1/2]

    Change-Id: I5987cf9fee6ea59532e8d7f2f484be9b1dfa9ee3

    From: Ido Ben-Hur <idoybh2@gmail.com>
    Date: Sun, 12 Feb 2023 15:49:51 +0200
    Subject: base: Allow controlling statusbar battery % display when
    charging [1/2]

    This actually kills 2 birds with 1 stone.
    User can control whether % jumps outside when inside (was default)
    and can show only while charging when % is disabled entirely

    Also improve some other code laying around
    simplify logic, remove useless checks and better document

Change-Id: I23798426e603b277408fb63f4898b385027e7eeb
Co-authored-by: Ido Ben-Hur <idoybh2@

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -78,6 +78,7 @@ public class SystemSettings {
         Settings.System.NOTIFICATION_SOUND,
         Settings.System.ACCELEROMETER_ROTATION,
         Settings.System.SHOW_BATTERY_PERCENT,
+        Settings.System.SHOW_BATTERY_PERCENT_CHARGING,
         Settings.System.ALARM_VIBRATION_INTENSITY,
         Settings.System.MEDIA_VIBRATION_INTENSITY,
         Settings.System.NOTIFICATION_VIBRATION_INTENSITY,
@@ -101,5 +102,8 @@ public class SystemSettings {
         Settings.System.CAMERA_FLASH_NOTIFICATION,
         Settings.System.SCREEN_FLASH_NOTIFICATION,
         Settings.System.SCREEN_FLASH_NOTIFICATION_COLOR,
+        Settings.System.QS_SHOW_BATTERY_ESTIMATE,
+        Settings.System.STATUS_BAR_BATTERY_STYLE,
+        Settings.System.SHOW_BATTERY_PERCENT_INSIDE,
     };
 }

TheXPerienceProject/platform_frameworks_base

    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Date:   Sat Aug 3 19:50:33 2024 +0300
    SystemUI: LandscapeBatteryDrawable: Further tune colors
    and improve bolt visibility / appearance
    Date:   Sat Aug 3 19:21:51 2024 +0300
    SystemUI: battery: Refactor battery style code
    Use OOP principles correctly here.
    Use polymorphism to avoid giant switch cases and doing the same thing for every drawable separately...
    Many other code / efficacy improvements
    Date:   Sat Aug 3 14:33:52 2024 +0300
    base: *LandscapeBatteryDrawable: Always use a single text color
    Add a background color with 75% alpha and +-20% color
    Always paint all of the text with the fill color inverse
    Date:   Sun Jul 28 02:41:47 2024 +0300
    LandscapeBatteryDrawable: Get rid of dual tone colors
    It breaks background fill in this style
    Date:   Wed Jul 31 12:13:25 2024 +0300
    CircleBatteryDrawable: Always calculate background off of fill color
    this will always be fill color with reduced alpha
    fixes some situations where it would look entirely filled like in the battery chip which sets black to all 3 colors
    Date:   Thu Jul 25 18:38:24 2024 +0300
    base: Make landscape battery style thicker
    so we can show a bigger text inside and to align with the rest of the icons
    also adjust font typeface so it's not too bold / thin
    Author: 703joko <703joko@gmail.com>
    Date:   Thu Jul 22 12:39:13 2021 +0700
    base: Add landscape battery style [1/2]
    Inspiration from Ancient ROM, thanks to Kakek for helps.
    neobuddy89 (crdroid):
    * Fix battery size dimensions
    * Fix percentage view on landscape
    idoybh (YAAP):
    * improved a lot of bad code
    * extend for reversed instead of rewriting everything
    * don't break estimates (+ other potential bug fixes)
    * adapted to our custom changes
    Change-Id: I4eabcaf1d70db2809beeb0a89051cb75fd320376
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Ido Ben-Hur

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -117,7 +117,10 @@ public class SystemSettings {
                 Settings.System.NOTIFICATION_COOLDOWN_ENABLED,
                 Settings.System.NOTIFICATION_COOLDOWN_ALL,
                 Settings.System.NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED,
-                Settings.System.LOCKSCREEN_ROTATION
+                Settings.System.LOCKSCREEN_ROTATION,
+                Settings.System.QS_SHOW_BATTERY_ESTIMATE,
+                Settings.System.STATUS_BAR_BATTERY_STYLE,
+                Settings.System.SHOW_BATTERY_PERCENT_INSIDE
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);