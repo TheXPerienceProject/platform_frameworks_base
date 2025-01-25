fc937757e2e907f1bf7e43e9c1f65aa732f9fbef
klozz <carlosj@klozz.dev>
2025-01-25 12:36:10 +0000

    Revert "Long-press power while display is off for torch"
    This reverts commit b0115e291fac3d85909b5cdc185c72f7381c73b9.
    
    Signed-off-by: klozz <carlosj@klozz.dev>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6354,25 +6354,6 @@ public final class Settings {
         public static final String STATUS_BAR_QUICK_QS_PULLDOWN = "qs_quick_pulldown";
 
         /**
-         * Activate torchlight when power button is
-         * long-pressed while the display is off
-         * The value is boolean (1 or 0).
-         * @hide
-         */
-        public static final String TORCH_LONG_PRESS_POWER_GESTURE =
-                "torch_long_press_power_gesture";
-
-        /**
-         * When the torch has been turned on by long press on power,
-         * automatically turn off after a configurable number of seconds.
-         * The value is an integer number of seconds in the range 0-3600.
-         * 0 means never automatically turn off.
-         * @hide
-         */
-        public static final String TORCH_LONG_PRESS_POWER_TIMEOUT =
-                "torch_long_press_power_timeout";
-
-        /**
          * @hide
          */
         public static final String ENABLE_TASKBAR = "enable_taskbar";
