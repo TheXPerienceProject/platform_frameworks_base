7b9c002caed8483d5f04e0ccedf5ca2bc90f657c
LuK1337 <priv.luk@gmail.com>
2025-03-24 10:47:02 +0000

    SystemUI: Add ENABLE_TASKBAR setting
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: klozz <carlosj@klozz.dev>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6774,6 +6774,11 @@ public final class Settings {
         public static final String SHOW_APP_VOLUME = "show_app_volume";
 
         /**
+          * Whether to enable taskbar.
+          */
+         public static final String ENABLE_TASKBAR = "enable_taskbar";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
