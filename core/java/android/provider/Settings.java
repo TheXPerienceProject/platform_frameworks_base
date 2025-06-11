d450fc7e091bdd513cfa0a89d6c4b9ecef80327f
Abhay Singh Gill <abhaygill017@gmail.com>
2025-06-11 15:41:40 +0530

    base: Allow user to provide keybox data [1/2]
    Handle PKCS#1 RSA keys for keybox spoofing as well
    
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: klozz <carlosj@klozz.dev>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13284,6 +13284,13 @@ public final class Settings {
         public static final String SCREEN_OFF_UDFPS_ENABLED = "screen_off_udfps_enabled";
 
         /**
+         * User selectable keybox data.
+         * @hide
+         */
+        @Readable
+        public static final String KEYBOX_DATA = "keybox_data";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
