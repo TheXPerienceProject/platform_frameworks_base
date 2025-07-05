de8ed3932ba3441ad673fae3d4820cd20aa411d7
klozz <carlosj@klozz.dev>
2025-07-04 19:16:01 -0600

    fix build
    Signed-off-by: klozz <carlosj@klozz.dev>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6561,6 +6561,12 @@ public final class Settings {
         public static final String ON_THE_GO_CAMERA = "on_the_go_camera";
 
         /**
+         * Whether to include options in power menu for rebooting into recovery or bootloader
+         * @hide
+         */
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
---
+++ core/java/com/android/internal/xperience/PowerMenuUtils.java
@@ -0,0 +1,25 @@
+/*
+ * SPDX-FileCopyrightText: 2015 The CyanogenMod Project
+ * SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.internal.xperience;
+
+import android.app.KeyguardManager;
+import android.content.Context;
+import android.os.UserHandle;
+
+import android.providers.Settings;
+
+public final class PowerMenuUtils {
+    public static boolean isAdvancedRestartPossible(final Context context) {
+        KeyguardManager km = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
+        boolean keyguardLocked = km.inKeyguardRestrictedInputMode() && km.isKeyguardSecure();
+        boolean advancedRestartEnabled = Settings.Secure.getInt(context.getContentResolver(),
+                Settings.Secure.ADVANCED_REBOOT, 1) == 1;
+        boolean isPrimaryUser = UserHandle.getCallingUserId() == UserHandle.USER_SYSTEM;
+
+        return advancedRestartEnabled && !keyguardLocked && isPrimaryUser;
+    }
+}
