78a895600e13ab79e272b72566502665a9e0688d
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-21 21:55:28 +0800

    SystemUI: Add edge light feature
    @neobuddy89:
    * Fix multi-user usage.
    * Define settings key we used earlier.
    
    Change-Id: I2bdf550eadd92d5f914b11ac48101eb8cf368e97
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: klozz <carlosj@klozz.dev>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -132,6 +132,7 @@ import com.android.systemui.dagger.qualifiers.UiBackground;
 import com.android.systemui.demomode.DemoMode;
 import com.android.systemui.demomode.DemoModeController;
 import com.android.systemui.display.dagger.SystemUIDisplaySubcomponent;
+import com.android.systemui.edgelight.EdgeLightViewController;
 import com.android.systemui.emergency.EmergencyGesture;
 import com.android.systemui.emergency.EmergencyGestureModule.EmergencyGestureIntentFactory;
 import com.android.systemui.flags.FeatureFlags;
@@ -486,6 +487,12 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
     private final WallpaperManager mWallpaperManager;
     private final UserTracker mUserTracker;
     private final ActivityStarter mActivityStarter;
+<<<<<<< HEAD
+=======
+    private final MediaViewController mMediaViewController;
+    private final PulseViewController mPulseViewController;
+    private final EdgeLightViewController mEdgeLightViewController;
+>>>>>>> 7630a1e185eb (SystemUI: Add edge light feature)
 
     private final DisplayMetrics mDisplayMetrics;
 
@@ -751,7 +758,13 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
             QuickAccessWalletController walletController,
             WindowManager windowManager,
             WindowManagerProvider windowManagerProvider,
+<<<<<<< HEAD
             BurnInProtectionController burnInProtectionController
+=======
+            MediaViewController mediaViewController,
+            PulseViewController pulseViewController,
+            EdgeLightViewController edgeLightViewController
+>>>>>>> 7630a1e185eb (SystemUI: Add edge light feature)
     ) {
         mContext = context;
         mNotificationsController = notificationsController;
@@ -899,8 +912,14 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
 
         mWindowManager = windowManager;
         mWindowManagerProvider = windowManagerProvider;
+<<<<<<< HEAD
 
         ScreenAnimationController.INSTANCE().init(new AmbientDisplayConfiguration(mContext));
+=======
+        mMediaViewController = mediaViewController;
+        mPulseViewController = pulseViewController;
+        mEdgeLightViewController = edgeLightViewController;
+>>>>>>> 7630a1e185eb (SystemUI: Add edge light feature)
     }
 
     private void initBubbles(Bubbles bubbles) {
@@ -1170,6 +1189,12 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
                 (requestTopUi, componentTag) -> mMainExecutor.execute(
                         () -> mTopUiController.setRequestTopUi(requestTopUi, componentTag)
                 )));
+<<<<<<< HEAD
+=======
+        getNotifContainerParentView().addView(mMediaViewController.getMediaArtScrim(), 0);
+        getNotifContainerParentView().addView(mPulseViewController.getPulseView(), 1);
+        getNotifContainerParentView().addView(mEdgeLightViewController.getEdgeLightView(), 2);
+>>>>>>> 7630a1e185eb (SystemUI: Add edge light feature)
     }
 
     public void destroy() {
