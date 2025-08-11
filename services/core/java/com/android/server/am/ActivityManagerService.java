AxionAOSP/android_frameworks_base

[PERF] activity resume boost
Change-Id: Ie844a41e634c456aca03455893d5bd8605cff95a
Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

services/core/java/com/android/server/am/ActivityManagerService.java
@@ -19623,7 +19623,7 @@ public class ActivityManagerService extends IActivityManager.Stub
         String path = null;
         String cpuset = null;
         String bgCpuset = SystemProperties.get("persist.sys.axion_cpu_limit_bg", "0-1");
-        String fgCpuSet = SystemProperties.get("persist.sys.axion_cpu_unlimit_ui", "0-7");
+        String fgCpuSet = SystemProperties.get("persist.sys.axion_cpu_limit_ui", "0-4");
 
         switch (cgroup) {
             case "bg":
@@ -19640,6 +19640,10 @@ public class ActivityManagerService extends IActivityManager.Stub
                 break;
             case "sys-bg":
                 path = "/dev/cpuset/system-background/cpus";
+                cpuset = bgCpuset;
+                break;
+            case "cam":
+                path = "/dev/cpuset/camera-daemon/cpus";
                 cpuset = bgCpuset;
                 break;
             default:
@@ -19799,7 +19803,8 @@ public class ActivityManagerService extends IActivityManager.Stub
         setPerformanceMode(true, reason);
 
         if (reason != null && reason.toLowerCase().contains("launch")) {
-            adjustCpusetCpus("fg", duration);
+            String fgboost = SystemProperties.get("persist.sys.axion_cpu_unlimit_ui", "0-7");
+            adjustCpusetCpus("/dev/cpuset/foreground/cpus", fgboost, duration);
         }
 
         adjustCpusetCpus("bg", duration);

TheXPerienceProject/platform_frameworks_base

Signed-off-by: Dmitrii <bankersenator@gmail.com>
Signed-off-by: klozz <carlosj@klozz.dev>

services/core/java/com/android/server/am/ActivityManagerService.java
@@ -19659,7 +19659,7 @@ public class ActivityManagerService extends IActivityManager.Stub
         String path = null;
         String cpuset = null;
         String bgCpuset = SystemProperties.get("persist.sys.xperience_cpu_limit_bg", "0-1");
-        String fgCpuSet = SystemProperties.get("persist.sys.xperience_cpu_unlimit_ui", "0-7");
+        String fgCpuSet = SystemProperties.get("persist.sys.xperience_cpu_unlimit_ui", "0-4");
 
         switch (cgroup) {
             case "bg":
@@ -19676,6 +19676,10 @@ public class ActivityManagerService extends IActivityManager.Stub
                 break;
             case "sys-bg":
                 path = "/dev/cpuset/system-background/cpus";
+                cpuset = bgCpuset;
+                break;
+            case "cam":
+                path = "/dev/cpuset/camera-daemon/cpus";
                 cpuset = bgCpuset;
                 break;
             default:
@@ -19835,7 +19839,8 @@ public class ActivityManagerService extends IActivityManager.Stub
         setPerformanceMode(true, reason);
 
         if (reason != null && reason.toLowerCase().contains("launch")) {
-            adjustCpusetCpus("fg", duration);
+            String fgboost = SystemProperties.get("persist.sys.xperience_cpu_unlimit_ui", "0-7");
+            adjustCpusetCpus("/dev/cpuset/foreground/cpus", fgboost, duration);
         }
 
         adjustCpusetCpus("bg", duration);