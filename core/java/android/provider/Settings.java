47ce09de70cb920f92d0e3a1eed6534ff69fb4d0
NurKeinNeid <nurkeinneid@derpfest.org>
2025-08-16 22:45:43 +0200

    core: Add timestamp constants for keybox and PIF data
    Add KEYBOX_DATA_TIMESTAMP and PIF_DATA_TIMESTAMP constants to store
    actual loading timestamps for keybox and PIF data, improving user
    experience by showing accurate loading times.
    
    Change-Id: Icd50ae358e8c8bfcebcd53ce4dbd43c2bcd8fd01
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>
    Signed-off-by: klozz <carlosj@klozz.dev>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13340,6 +13340,13 @@ public final class Settings {
         public static final String PIF_DATA = "pif_data";
 
         /**
+         * User selectable PIF data.
+         * @hide
+         */
+        @Readable
+        public static final String PIF_DATA_TIMESTAMP = "pif_data_timestamp";
+
+        /**
          * PIF data fetched from our repo.
          * @hide
          */
@@ -13352,6 +13359,13 @@ public final class Settings {
          */
         @Readable
         public static final String KEYBOX_DATA = "keybox_data";
+
+        /**
+         * User selectable keybox data.
+         * @hide
+         */
+        @Readable
+        public static final String KEYBOX_DATA_TIMESTAMP = "keybox_data_timestamp";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
