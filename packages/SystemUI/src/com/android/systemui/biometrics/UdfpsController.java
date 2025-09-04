86283420227e585f185f0dc4e1e215cb88803dc2
Michael Bestas <mkbestas@lineageos.org>
2025-09-04 12:06:19 +0300

    fixup! SystemUI: Add support for udfps dim layer
    Change-Id: Ib705ac06a125f12ea5a7c87ec06e0b4f72d637f3

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -316,6 +316,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
             mUdfpsOverlayInteractor.setRequestId(requestId);
             mFgExecutor.execute(() -> UdfpsController.this.showUdfpsOverlay(
                     new UdfpsControllerOverlay(
+                        mContext,
                         mInflater,
                         mWindowManager,
                         mAccessibilityManager,
@@ -333,6 +334,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                         mDeviceEntryUdfpsTouchOverlayViewModel,
                         mDefaultUdfpsTouchOverlayViewModel,
                         mPromptUdfpsTouchOverlayViewModel,
+                        mShadeInteractor,
                         mUdfpsOverlayInteractor,
                         mPowerInteractor,
                         mScope
