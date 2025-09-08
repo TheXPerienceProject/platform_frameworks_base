HELLBOY017/aospa_frameworks_base

base: Add the ability to generate key pairs for keybox spoofing
The code is used from :-
https://github.com/5ec1cff/TrickyStore

Needs :-
https://github.com/HELLBOY017/external_bouncycastle/commit/f70ea016239bccf2d3f4b60b79a7165ef3d047be

This allows us to enjoy spoofing keyboxes the right way even with broken TEE.

Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

keystore/java/android/security/keystore2/AndroidKeyStoreKeyPairGeneratorSpi.java
@@ -54,6 +54,8 @@ import android.text.TextUtils;
 import android.util.ArraySet;
 import android.util.Log;
 
+import com.android.internal.util.neoteric.KeyboxImitationHooks;
+
 import libcore.util.EmptyArray;
 
 import java.math.BigInteger;
@@ -694,10 +696,24 @@ public abstract class AndroidKeyStoreKeyPairGeneratorSpi extends KeyPairGenerato
         boolean success = false;
         try {
             KeyStoreSecurityLevel iSecurityLevel = mKeyStore.getSecurityLevel(securityLevel);
-
-            KeyMetadata metadata = iSecurityLevel.generateKey(descriptor, mAttestKeyDescriptor,
-                    constructKeyGenerationArguments(), flags, additionalEntropy);
-
+            KeyMetadata metadata;
+            if (mSpec.getAttestationChallenge() != null &&
+                (KeyProperties.KEY_ALGORITHM_EC.equals(mJcaKeyAlgorithm) ||
+                 KeyProperties.KEY_ALGORITHM_RSA.equals(mJcaKeyAlgorithm))) {
+                metadata = KeyboxImitationHooks.generateKey(
+                    iSecurityLevel.getBinderInterface(),
+                    descriptor,
+                    constructKeyGenerationArguments()
+                );
+                if (metadata == null) {
+                    KeyboxImitationHooks.setFailFlag(true);
+                    metadata = iSecurityLevel.generateKey(descriptor, mAttestKeyDescriptor,
+                        constructKeyGenerationArguments(), flags, additionalEntropy);
+                }
+            } else {
+                metadata = iSecurityLevel.generateKey(descriptor, mAttestKeyDescriptor,
+                        constructKeyGenerationArguments(), flags, additionalEntropy);
+            }
             AndroidKeyStorePublicKey publicKey =
                     AndroidKeyStoreProvider.makeAndroidKeyStorePublicKeyFromKeyEntryResponse(
                             descriptor, metadata, iSecurityLevel, mKeymasterAlgorithm);

TheXPerienceProject/platform_frameworks_base

keystore/java/android/security/keystore2/AndroidKeyStoreKeyPairGeneratorSpi.java
@@ -55,6 +55,8 @@ import android.text.TextUtils;
 import android.util.ArraySet;
 import android.util.Log;
 
+import com.android.internal.util.derpfest.KeyboxImitationHooks;
+
 import libcore.util.EmptyArray;
 
 import java.math.BigInteger;
@@ -695,10 +697,24 @@ public abstract class AndroidKeyStoreKeyPairGeneratorSpi extends KeyPairGenerato
         boolean success = false;
         try {
             KeyStoreSecurityLevel iSecurityLevel = mKeyStore.getSecurityLevel(securityLevel);
-
-            KeyMetadata metadata = iSecurityLevel.generateKey(descriptor, mAttestKeyDescriptor,
-                    constructKeyGenerationArguments(), flags, additionalEntropy);
-
+            KeyMetadata metadata;
+            if (mSpec.getAttestationChallenge() != null &&
+                (KeyProperties.KEY_ALGORITHM_EC.equals(mJcaKeyAlgorithm) ||
+                 KeyProperties.KEY_ALGORITHM_RSA.equals(mJcaKeyAlgorithm))) {
+                metadata = KeyboxImitationHooks.generateKey(
+                    iSecurityLevel.getBinderInterface(),
+                    descriptor,
+                    constructKeyGenerationArguments()
+                );
+                if (metadata == null) {
+                    KeyboxImitationHooks.setFailFlag(true);
+                    metadata = iSecurityLevel.generateKey(descriptor, mAttestKeyDescriptor,
+                        constructKeyGenerationArguments(), flags, additionalEntropy);
+                }
+            } else {
+                metadata = iSecurityLevel.generateKey(descriptor, mAttestKeyDescriptor,
+                        constructKeyGenerationArguments(), flags, additionalEntropy);
+            }
             AndroidKeyStorePublicKey publicKey =
                     AndroidKeyStoreProvider.makeAndroidKeyStorePublicKeyFromKeyEntryResponse(
                             descriptor, metadata, iSecurityLevel, mKeymasterAlgorithm);