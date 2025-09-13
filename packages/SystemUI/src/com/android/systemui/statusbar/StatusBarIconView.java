9e68d6b5b2299714b0b21d95506f19c920307fa8
klozz <carlosj@klozz.dev>
2025-09-12 22:17:14 -0600

    SystemUI: RefreshRate icon with auto-setting update
    This commit implements a complete refresh rate indicator system
    that automatically updates without requiring SystemUI restart.
    
    The indicator can be toggled via:
    adb shell settings put system show_refresh_rate [0|1]
    
    Change-Id: I233481cf513a01fd5588a06cca9160aa46003f11
    Signed-off-by: klozz <carlosj@klozz.dev>

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java
@@ -1116,7 +1116,7 @@ public class StatusBarIconView extends AnimatedImageView implements StatusIconDi
                     R.dimen.status_bar_refresh_rate_padding);
 
             setPadding(
-                    getPaddingLeft() + extraPadding, // left only
+                    extraPadding,
                     getPaddingTop(),
                     getPaddingRight(),
                     getPaddingBottom()
