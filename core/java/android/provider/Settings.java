463e04f511286fd52a5377c64b5ccedee6f53416
Ghosuto <clash.raja10@gmail.com>
2025-10-09 05:00:27 +0000

    SystemUI: Dynamic charging ripple switch [1/2]
    Change-Id: Idb130a09b9eb8a0ef4aea1f0646b41dea6840b21
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>
    Signed-off-by: klozz <carlosj@klozz.dev>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7190,6 +7190,12 @@ public final class Settings {
         public static final String UDFPS_ICON = "udfps_icon";
 
         /**
+         * Use dynamic color based on battery level
+         * @hide
+         */
+        public static final String WIRELESS_CHARGING_DYNAMIC_COLOR = "wireless_charging_dynamic_color";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
