da2153c490c88b38b0972493cdede522be9f24f3
klozz <carlosj@klozz.dev>
2025-10-23 23:44:32 -0600

    Revert "core: Add timestamp constants for keybox and PIF data"
    This reverts commit f2e64c95d5f41d38732362625815210c3b3cf9b6.
    
    Change-Id: I3684b84703ca75c6fd67565fec2f7d8d497107db
    Signed-off-by: klozz <carlosj@klozz.dev>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13334,13 +13334,6 @@ public final class Settings {
         public static final String PIF_DATA = "pif_data";
 
         /**
-         * User selectable PIF data.
-         * @hide
-         */
-        @Readable
-        public static final String PIF_DATA_TIMESTAMP = "pif_data_timestamp";
-
-        /**
          * PIF data fetched from our repo.
          * @hide
          */
@@ -13355,13 +13348,6 @@ public final class Settings {
         public static final String KEYBOX_DATA = "keybox_data";
 
         /**
-         * User selectable keybox data.
-         * @hide
-         */
-        @Readable
-        public static final String KEYBOX_DATA_TIMESTAMP = "keybox_data_timestamp";
-
-	/**
          * Whether to pulse ambient on new music tracks
          * @hide
          */
