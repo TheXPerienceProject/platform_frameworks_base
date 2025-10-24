81218362246eee600ffe737d176be670a5fd8e56
klozz <carlosj@klozz.dev>
2025-10-23 23:48:53 -0600

    Revert "gmscompat: Add prop to disable gms spoofing"
    This reverts commit 38bed1152dd29f3a9b02b5b3ab4a3d352b410638.

--- services/core/java/com/android/server/gmscompat/AttestationService.java
+++ services/core/java/com/android/server/gmscompat/AttestationService.java
@@ -40,8 +40,6 @@ public final class AttestationService extends SystemService {
     private static final long INTERVAL = 5;
 
     private static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
-    private static final boolean SPOOF_GMS =
-            SystemProperties.getBoolean("persist.sys.spoof.gms", true);
 
     private final Context mContext;
     private final File mDataFile;
@@ -59,7 +57,7 @@ public final class AttestationService extends SystemService {
 
     @Override
     public void onBootPhase(int phase) {
-        if (SPOOF_GMS && phase == PHASE_BOOT_COMPLETED) {
+        if (phase == PHASE_BOOT_COMPLETED) {
             Log.i(TAG, "Scheduling the service");
             mScheduler.scheduleAtFixedRate(
                     new FetchGmsCertifiedProps(), INITIAL_DELAY, INTERVAL, TimeUnit.MINUTES);
