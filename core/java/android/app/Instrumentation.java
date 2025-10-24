b9591b21bf4d1e20709241c6a70f4ec0f30c2dd5
klozz <carlosj@klozz.dev>
2025-10-23 23:48:58 -0600

    Revert "gmscompat: Get package name from context instead of app"
    This reverts commit 5291f59641ab9c767c5f7104d2775fd07a1bfdcb.

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -1348,7 +1348,7 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
-        AttestationHooks.initApplicationBeforeOnCreate(context);
+        AttestationHooks.initApplicationBeforeOnCreate(app);
         return app;
     }
     
@@ -1366,7 +1366,7 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
-        AttestationHooks.initApplicationBeforeOnCreate(context);
+        AttestationHooks.initApplicationBeforeOnCreate(app);
         return app;
     }
 
