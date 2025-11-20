65c33a94ba4c05e8ab9ca81e887ccc960f195f65
Ritesh <SahanyRitesh@proton.me>
2025-11-20 15:26:01 +0530

    base: Refactor three-finger swipe screenshot
    Change-Id: I36bfb22d3e1ebf41b95144f054aa372f5431ef18
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: klozz <carlosj@klozz.dev>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -479,7 +479,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     DisplayManagerInternal mDisplayManagerInternal;
     UserManagerInternal mUserManagerInternal;
     DockObserverInternal mDockObserverInternal;
-    KeyGestureController mKeyGestureController;
 
     private WallpaperManagerInternal mWallpaperManagerInternal;
 
@@ -2993,12 +2992,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 mRingerToggleChord = VOLUME_HUSH_OFF;
             }
 
-            // Three Finger Gesture
-            boolean threeFingerGesture = Settings.System.getIntForUser(resolver,
-                    Settings.System.SWIPE_TO_SCREENSHOT, 0, UserHandle.USER_CURRENT) == 1;
-            if (mKeyGestureController != null) {
-                mKeyGestureController.enableSwipeThreeFingerGesture(threeFingerGesture);
-            }
 
             // Configure wake gesture.
             boolean wakeGestureEnabledSetting = Settings.Secure.getIntForUser(resolver,
