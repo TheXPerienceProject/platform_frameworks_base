ab6063dd6905abc9b96a027ac301f23aaf570657
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-11-27 17:29:54 +0800

    SystemUI: Redesigning media panel
    Change-Id: I527fbd77a5074a3b861edbc508fcfde3d098b827
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/media/controls/ui/controller/MediaControlPanel.java
+++ packages/SystemUI/src/com/android/systemui/media/controls/ui/controller/MediaControlPanel.java
@@ -1395,7 +1395,7 @@ public class MediaControlPanel {
                     @Override
                     protected float getCurrentTopCornerRadius() {
                         return mContext.getResources().getDimension(
-                                R.dimen.notification_corner_radius);
+                                R.dimen.media_notification_corner_radius);
                     }
 
                     @Override
