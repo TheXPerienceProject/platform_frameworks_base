0a074e36167b4b52e7e2db7a4fb9c7d47a85ab5d
klozz <carlosj@klozz.dev>
2025-11-28 01:48:39 -0600

    Revert "FlashlightControllerImpl: Fix crash when camera IDs list is null"
    This reverts commit 3f84590c835bd71cef2641fbd35e644a9350eb0e.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/FlashlightControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/FlashlightControllerImpl.java
@@ -181,11 +181,7 @@ public class FlashlightControllerImpl implements FlashlightController {
 
     @WorkerThread
     private String getCameraId() throws CameraAccessException {
-        String[] ids = mCameraManager == null ? null : mCameraManager.getCameraIdList();
-        if (ids == null || ids.length == 0) {
-            Log.e(TAG, "Camera ID list is null or empty.");
-            return null;
-        }
+        String[] ids = mCameraManager.getCameraIdList();
         for (String id : ids) {
             CameraCharacteristics c = mCameraManager.getCameraCharacteristics(id);
             Boolean flashAvailable = c.get(CameraCharacteristics.FLASH_INFO_AVAILABLE);
