99114048f9973da12c06f13ca8aff8150ff5b823
Pranav Vashi <neobuddy89@gmail.com>
2025-12-14 23:21:46 +0530

    SystemUI: Add customization to show battery percent
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Change-Id: Ic14d30987eac9b412684db987698e224b93a8071
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: klozz <carlosj@klozz.dev>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -129,7 +129,8 @@ public class SystemSettings {
                 Settings.System.PREFERRED_REGION,
                 Settings.System.CV_ENABLED,
                 Settings.System.CV_DYNAMIC_ENABLED,
-                Settings.System.CV_PREFERRED_INTENSITY
+                Settings.System.CV_PREFERRED_INTENSITY,
+                Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);
