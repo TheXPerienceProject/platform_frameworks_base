crdroidandroid/android_frameworks_base

Revert "SystemUI: Fix thread safety issue when adding tunables"
This reverts commit f1f425e8d4f8eaba1f1c20bdd4cdd9ac261156ee.

packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
@@ -342,12 +342,10 @@ public class TunerServiceImpl extends TunerService {
         } else {
             uri = Settings.Secure.getUriFor(key);
         }
-        synchronized (this) {
-            if (!mListeningUris.containsKey(uri)) {
-                mListeningUris.put(uri, key);
-                mContentResolver.registerContentObserver(uri, false, mObserver,
-                        isLineageGlobal(key) ? UserHandle.USER_ALL : mCurrentUser);
-            }
+        if (!mListeningUris.containsKey(uri)) {
+            mListeningUris.put(uri, key);
+            mContentResolver.registerContentObserver(uri, false, mObserver,
+                    isLineageGlobal(key) ? UserHandle.USER_ALL : mCurrentUser);
         }
         // Send the first state.
         String value = DejankUtils.whitelistIpcs(() -> getValue(key));

TheXPerienceProject/platform_frameworks_base

Signed-off-by: Dmitrii <bankersenator@gmail.com>

packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
@@ -324,12 +324,10 @@ public class TunerServiceImpl extends TunerService {
         } else {
             uri = Settings.Secure.getUriFor(key);
         }
-        synchronized (this) {
-            if (!mListeningUris.containsKey(uri)) {
-                mListeningUris.put(uri, key);
-                mContentResolver.registerContentObserver(uri, false, mObserver,
-                        isCustomGlobal(key) ? UserHandle.USER_ALL : mCurrentUser);
-            }
+        if (!mListeningUris.containsKey(uri)) {
+            mListeningUris.put(uri, key);
+            mContentResolver.registerContentObserver(uri, false, mObserver,
+                    isCustomGlobal(key) ? UserHandle.USER_ALL : mCurrentUser);
         }
         // Send the first state.
         String value = DejankUtils.whitelistIpcs(() -> getValue(key));