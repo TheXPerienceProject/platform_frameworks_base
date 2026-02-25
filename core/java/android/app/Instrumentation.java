15a0a76d4a26ab7119ac72d7103ce9fc114e94e4
klozz <carlosj@klozz.dev>
2026-02-25 01:55:01 -0600

    fix build
    Change-Id: I3be72f8110fa93d7937e71e9f212e37ff4b28853
    Signed-off-by: klozz <carlosj@klozz.dev>

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -80,8 +80,8 @@ import java.util.Objects;
 import java.util.StringJoiner;
 import java.util.concurrent.TimeoutException;
 
-import com.android.internal.util.voltage.AttestationHooks;
-import com.android.internal.util.voltage.PixelPropsUtils;
+import com.android.internal.util.xperience.AttestationHooks;
+import com.android.internal.util.xperience.PixelPropsUtils;
 
 /**
  * Base class for implementing application instrumentation code.  When running
