e9396f85b4a7b128f9d030278884d61dab8e77d0
klozz <carlosj@klozz.dev>
2026-02-26 17:00:11 -0600

    fix build
    Change-Id: I8146e839f99be985e113ebbf3902d62e65ea7a11
    Signed-off-by: klozz <carlosj@klozz.dev>

--- services/core/java/com/android/server/xperience/VbmetaHashService.java
+++ services/core/java/com/android/server/xperience/VbmetaHashService.java
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  *
  */
-package com.android.server.voltage;
+package com.android.server.xperience;
 
 import android.annotation.Nullable;
 import android.content.Context;
