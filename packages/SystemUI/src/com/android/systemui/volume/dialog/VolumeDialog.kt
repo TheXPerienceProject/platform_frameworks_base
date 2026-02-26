HaloUI-AOSP/android_frameworks_base

VolumeDialog: Do not leak content observer
Change-Id: Iba138e3ddc857c35e529953e05d0e44094604199
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/volume/dialog/VolumeDialog.kt
@@ -109,15 +109,6 @@ constructor(
                 }
         }
 
-        context.contentResolver.registerContentObserver(
-            LineageSettings.Secure.getUriFor(LineageSettings.Secure.VOLUME_PANEL_ON_LEFT),
-            false,
-            volumePanelOnLeftObserver,
-            UserHandle.USER_ALL
-        )
-        volumePanelOnLeftObserver.onChange(true)
-        applyLayoutAndGravity()
-
         setCancelable(false)
         setCanceledOnTouchOutside(true)
     }
@@ -139,6 +130,29 @@ constructor(
                 awaitCancellation()
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        context.contentResolver.registerContentObserver(
+            LineageSettings.Secure.getUriFor(LineageSettings.Secure.VOLUME_PANEL_ON_LEFT),
+            false,
+            volumePanelOnLeftObserver,
+            UserHandle.USER_ALL
+        )
+        volumePanelOnLeft = LineageSettings.Secure.getIntForUser(
+            context.contentResolver,
+            LineageSettings.Secure.VOLUME_PANEL_ON_LEFT,
+            0,
+            UserHandle.USER_CURRENT
+        ) != 0
+        applyLayoutAndGravity()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        context.contentResolver.unregisterContentObserver(volumePanelOnLeftObserver)
     }
 
     /**

TheXPerienceProject/platform_frameworks_base

Signed-off-by: Dmitrii <bankersenator@gmail.com>

packages/SystemUI/src/com/android/systemui/volume/dialog/VolumeDialog.kt
@@ -108,15 +108,6 @@ constructor(
                 }
         }
 
-        context.contentResolver.registerContentObserver(
-            Settings.Secure.getUriFor(Settings.Secure.VOLUME_PANEL_ON_LEFT),
-            false,
-            volumePanelOnLeftObserver,
-            UserHandle.USER_ALL
-        )
-        volumePanelOnLeftObserver.onChange(true)
-        applyLayoutAndGravity()
-
         setCancelable(false)
         setCanceledOnTouchOutside(false)
     }
@@ -138,6 +129,26 @@ constructor(
                 awaitCancellation()
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        context.contentResolver.registerContentObserver(
+            Settings.Secure.getUriFor(Settings.Secure.VOLUME_PANEL_ON_LEFT),
+            false,
+            volumePanelOnLeftObserver,
+            UserHandle.USER_ALL
+        )
+        volumePanelOnLeft = Settings.Secure.getIntForUser(
+            context.contentResolver, Settings.Secure.VOLUME_PANEL_ON_LEFT,
+            0, UserHandle.USER_CURRENT
+        ) != 0
+        applyLayoutAndGravity()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        context.contentResolver.unregisterContentObserver(volumePanelOnLeftObserver)
     }
 
     /**