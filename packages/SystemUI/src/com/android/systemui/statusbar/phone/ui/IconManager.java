ba6ffaefe9455c8501d48ef853c3d8ae977b9491
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2026-03-13 13:18:21 +0530

    SystemUI: Allow toggling VoLTE/VoNR & VoWiFi icons
    * Settings >> Display >> Icon manager
    * Preview: https://ibb.co/4gVWHtr9
    
    Change-Id: I7e59fd76f304c8ffa42b6e2253c5e147aeabf5fc
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>
    Signed-off-by: klozz <carlosj@klozz.dev>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ui/IconManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ui/IconManager.java
@@ -182,7 +182,7 @@ public class IconManager implements DemoModeCommandReceiver {
             case TYPE_NETWORK_TRAFFIC -> addNetworkTraffic(index, slot, holder.getNetworkTrafficState());
             case TYPE_BINDABLE ->
                 // Safe cast, since only BindableIconHolders can set this tag on themselves
-                    addBindableIcon((BindableIconHolder) holder, index);
+                    addBindableIcon((BindableIconHolder) holder, index, blocked);
             default -> null;
         };
     }
@@ -202,9 +202,10 @@ public class IconManager implements DemoModeCommandReceiver {
      * ViewBinder to control its visual state.
      */
     protected StatusIconDisplayable addBindableIcon(BindableIconHolder holder,
-            int index) {
+            int index, boolean blocked) {
         mBindableIcons.put(holder.getSlot(), holder);
         ModernStatusBarView view = holder.getInitializer().createAndBind(mContext);
+        view.setIconBlocked(blocked);
         mGroup.addView(view, index, onCreateLayoutParams(Shape.WRAP_CONTENT));
         if (mIsInDemoMode) {
             mDemoStatusIcons.addBindableIcon(holder);
