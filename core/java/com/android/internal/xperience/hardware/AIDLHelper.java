The-Clover-Project/frameworks_base

base: Wire up AIDL touch HAL
Change-Id: Ic1c24f10f8ca6282270660f11987b57353548efc

core/java/com/android/internal/lineage/hardware/AIDLHelper.java
@@ -1,0 +1,29 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.internal.lineage.hardware;
+
+import java.util.ArrayList;
+
+class AIDLHelper {
+    static TouchscreenGesture[] fromAIDLGestures(
+            vendor.lineage.touch.Gesture[] gestures) {
+        int size = gestures.length;
+        TouchscreenGesture[] r = new TouchscreenGesture[size];
+        for (int i = 0; i < size; i++) {
+            vendor.lineage.touch.Gesture g = gestures[i];
+            r[i] = new TouchscreenGesture(g.id, g.name, g.keycode);
+        }
+        return r;
+    }
+
+    static vendor.lineage.touch.Gesture toAIDLGesture(TouchscreenGesture gesture) {
+        vendor.lineage.touch.Gesture g = new vendor.lineage.touch.Gesture();
+        g.id = gesture.id;
+        g.name = gesture.name;
+        g.keycode = gesture.keycode;
+        return g;
+    }
+}

TheXPerienceProject/platform_frameworks_base

Signed-off-by: Dmitrii <bankersenator@gmail.com>
Signed-off-by: klozz <carlosj@klozz.dev>

core/java/com/android/internal/xperience/hardware/AIDLHelper.java
@@ -1,0 +1,29 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.internal.xperience.hardware;
+
+import java.util.ArrayList;
+
+class AIDLHelper {
+    static TouchscreenGesture[] fromAIDLGestures(
+            vendor.lineage.touch.Gesture[] gestures) {
+        int size = gestures.length;
+        TouchscreenGesture[] r = new TouchscreenGesture[size];
+        for (int i = 0; i < size; i++) {
+            vendor.lineage.touch.Gesture g = gestures[i];
+            r[i] = new TouchscreenGesture(g.id, g.name, g.keycode);
+        }
+        return r;
+    }
+
+    static vendor.lineage.touch.Gesture toAIDLGesture(TouchscreenGesture gesture) {
+        vendor.lineage.touch.Gesture g = new vendor.lineage.touch.Gesture();
+        g.id = gesture.id;
+        g.name = gesture.name;
+        g.keycode = gesture.keycode;
+        return g;
+    }
+}