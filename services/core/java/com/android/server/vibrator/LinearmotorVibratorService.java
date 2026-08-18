b88fc4e0e57af47e10430510b97d6653ea2a1ee5
klozz <carlosj@klozz.dev>
2026-08-18 11:10:07 +0000

    Revert "Enhance LinearmotorVibratorService with new methods"
    This reverts commit 1ba81f4e3d3fc2053c66e6034acf50598c1ee689.

--- services/core/java/com/android/server/vibrator/LinearmotorVibratorService.java
+++ services/core/java/com/android/server/vibrator/LinearmotorVibratorService.java
@@ -7,7 +7,6 @@ package com.android.server.vibrator;
 
 import android.content.Context;
 import android.os.Binder;
-import android.os.IBinder;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.util.Slog;
@@ -16,7 +15,6 @@ import com.android.server.SystemService;
 
 import com.oplus.os.ILinearmotorVibratorService;
 import com.oplus.os.WaveformEffect;
-
 
 public class LinearmotorVibratorService extends SystemService {
 
@@ -31,7 +29,7 @@ public class LinearmotorVibratorService extends SystemService {
 
     private final ILinearmotorVibratorService.Stub mService = new ILinearmotorVibratorService.Stub() {
         @Override
-        public void vibrate(int uid, String opPkg, WaveformEffect effect, IBinder token) {
+        public void vibrate(WaveformEffect effect) {
             synchronized (mLock) {
                 final long ident = Binder.clearCallingIdentity();
                 try {
@@ -42,58 +40,6 @@ public class LinearmotorVibratorService extends SystemService {
                     Binder.restoreCallingIdentity(ident);
                 }
             }
-        }
-
-        @Override
-        public void cancelVibrate(WaveformEffect effect, IBinder token) {
-            synchronized (mLock) {
-                mVibrator.cancel();
-            }
-        }
-
-        @Override
-        public int getVibratorStatus() {
-            return 0; // always ready
-        }
-
-        @Override
-        public void setVibratorStrength(int strength) {
-            // no-op
-        }
-
-        @Override
-        public int getSettingsTouchEffectStrength() {
-            return 2400;
-        }
-
-        @Override
-        public int getSettingsRingtoneEffectStrength() {
-            return 2400;
-        }
-
-        @Override
-        public int getSettingsNotificationEffectStrength() {
-            return 2400;
-        }
-
-        @Override
-        public int getVibratorTouchStyle() {
-            return 0;
-        }
-
-        @Override
-        public void setVibratorTouchStyle(int style) {
-            // no-op
-        }
-
-        @Override
-        public void updateVibrationAmplitude(int uid, String opPkg, float amplitudeRatio) {
-            // no-op
-        }
-
-        @Override
-        public boolean checkRichtapBlackList(String packageName) {
-            return false;
         }
     };
 
