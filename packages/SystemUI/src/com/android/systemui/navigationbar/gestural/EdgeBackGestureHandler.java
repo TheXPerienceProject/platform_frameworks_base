362af6a3ff7c039e769ff9bd92f7a1849715d7c9
klozz <carlosj@klozz.dev>
2026-09-12 20:52:29 -0600

    Revert "Cancel long swipe for cancelBack in navigation gesture"
    This reverts commit 6f3a18c773a48ee11ad13f4e51b2d0230fed71f5.
    
    i forgot to backport the lineage stuff and im lazy to do

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -347,7 +347,6 @@ public class EdgeBackGestureHandler {
                 public void cancelBack() {
                     if (mBackAnimation != null) {
                         mBackAnimation.setTriggerBack(false);
-                        mBackAnimation.setTriggerLongSwipe(false);
                     }
                     logGesture(SysUiStatsLog.BACK_GESTURE__TYPE__INCOMPLETE);
                 }
