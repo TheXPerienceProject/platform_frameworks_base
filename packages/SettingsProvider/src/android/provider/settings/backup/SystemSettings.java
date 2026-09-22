DerpFest-AOSP/android17

SystemUI: Introduce Dynamic Island [1/3]
refactor old music ticker code and adapt it to the dynamic island while at it

the island can be used to view and alter status of following
- music playback
- live scores in sports
- screen recording status
- flash light
- stop watch
- alarm

ref: Ieec1869bac37104cd8733278865cf44e773b8971

[nurkeinneid: Keep the island under quickactions/island, host it on
scene-container StatusBarRoot, and use Settings.System instead of
LineageSettings.]

Co-authored-by: NurKeinNeid <nurkeinneid@derpfest.org>
Change-Id: Ia833599db7747e83a9ce3ce2e10866bbe612ffe4
Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -140,7 +140,8 @@ public class SystemSettings {
                 Settings.System.QS_BRIGHTNESS_GRADIENT_ENABLED,
                 Settings.System.QS_VOLUME_GRADIENT_ENABLED,
                 Settings.System.GRADIENT_START_COLOR,
-                Settings.System.GRADIENT_END_COLOR
+                Settings.System.GRADIENT_END_COLOR,
+                Settings.System.STATUS_BAR_SHOW_DYNAMIC_ISLAND
         ));
         return settings.toArray(new String[0]);
     }

TheXPerienceProject/platform_frameworks_base

Signed-off-by: klozz <carlosj@klozz.dev>

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -132,7 +132,8 @@ public class SystemSettings {
                 Settings.System.CV_PREFERRED_INTENSITY,
                 Settings.System.PEAK_REFRESH_RATE,
                 Settings.System.MIN_REFRESH_RATE,
-                Settings.System.ACCESSIBILITY_FORCE_INVERT_COLOR_OVERRIDE_PACKAGES_TO_DISABLE
+                Settings.System.ACCESSIBILITY_FORCE_INVERT_COLOR_OVERRIDE_PACKAGES_TO_DISABLE,
+                Settings.System.STATUS_BAR_SHOW_DYNAMIC_ISLAND
         ));
         return settings.toArray(new String[0]);
     }